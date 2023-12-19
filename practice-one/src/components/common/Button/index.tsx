70a9444f886d22f14b65660cce45d49171ed4573
ngocphong230801 <nnphong962015@gmail.com>
2023-12-19 16:16:42 +0700

    Add props for button

--- practice-one/src/components/common/Button/index.tsx
+++ practice-one/src/components/common/Button/index.tsx
@@ -1,21 +1,27 @@
 import "./Button.css"
-import React, {MouseEventHandler, ReactNode} from "react";
+import React, { MouseEventHandler, ReactNode } from "react";
 import { Variant } from "../../../types/variant";
 
 type CustomBtnProps = {
     className?: string;
     children: ReactNode;
-    variant?:Variant;
+    variant?: Variant;
+    disabled?: boolean;
+    loading?: boolean;
     onClick: MouseEventHandler<HTMLButtonElement>;
 };
-const Button : React.FC<CustomBtnProps> = ({
+
+const Button: React.FC<CustomBtnProps> = ({
     className,
     onClick,
+    disabled,
     variant = Variant.DEFAULT,
     children,
-}) : React.ReactElement => {
-    return(
-        <button className={`${className} btn btn-${variant} `} onClick={onClick}>{children}</button>
-    )
-}
+    loading,
+}) => (
+    <button className={`${className} btn btn-${variant}`} onClick={onClick} disabled={disabled || loading}>
+        {loading ? "Loading..." : children}
+    </button>
+);
+
 export default Button
