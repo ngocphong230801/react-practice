306b5b24a4076630acd79e6010e19ff1e7acecb8
ngocphong230801 <nnphong962015@gmail.com>
2023-12-20 17:29:17 +0700

    Define header component

---
+++ practice-one/src/components/common/Header/Header.stories.tsx
@@ -0,0 +1,32 @@
+import Header from "./Header.css";
+import { Meta, StoryObj } from "@storybook/react";
+import Button from "../Button";
+import { Notify } from "../../Icon";
+import { THeader } from "../../../types";
+
+export default {
+    title: "Components/Header",
+    component: Header,
+} as Meta;
+
+type Story = StoryObj<typeof Header>;
+
+export const Default: Story = {
+    args: {
+        className: THeader.SECONDARY,
+        title: "Learn how to launch faster",
+        content : "watch our webinar for tips from our experts and get a limited time offer.",
+        icon: <Notify />,
+        children: <Button title = "Log out"/>
+    },
+};
+
+export const Primary: Story = {
+    args: {
+        className: THeader.PRIMARY,
+        title: "Learn how to launch faster",
+        content : "watch our webinar for tips from our experts and get a limited time offer.",
+        icon: <Notify />,
+        logOut: "Log out",
+    },
+};
