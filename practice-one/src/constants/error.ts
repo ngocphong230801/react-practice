43b6b5da60b25cb866931b4eda94363fb8c6ff97
ngocphong230801 <nnphong962015@gmail.com>
2023-12-26 18:00:14 +0700

    Init helper and update constant

---
+++ practice-one/src/constants/error.ts
@@ -0,0 +1,7 @@
+export const ERROR_MESSAGES = {
+    NAME: "Invalid name format, Ex: Le Van A",
+    EMAIL: "Invalid email format, Ex: lht@gmail.com",
+    PHONE: "Invalid phone format, Ex: 0365 xxx xxx",
+    PASSWORLD: "Invalid passworld format, Ex: 2382001Ph",
+    FIELD_EMPTY: "This field cannot be left blank",
+};
